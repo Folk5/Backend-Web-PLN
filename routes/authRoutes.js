2d99bfecb80ccc302b5550fc9b3d0acbe95aec1f
Folk5 <farrellapriansyah344@gmail.com>
2026-05-05 09:59:25 +0700

    docs: add comment explaining closed registration on /register route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,6 +6,9 @@ const requireAuth = require('../middleware/authMiddleware');
 const validateRequest = require('../middleware/validateRequest');
 const authValidator = require('../middleware/validators/authValidator');
 
+// Catatan: Route /register membutuhkan middleware requireAuth karena sistem 
+// ini menggunakan closed registration (hanya admin yang sudah login yang 
+// bisa membuat akun baru).
 router.post('/register', authLimiter, requireAuth, authValidator.register, validateRequest, authController.register);
 router.post('/login',    loginLimiter, authValidator.login, validateRequest, authController.login);
 router.post('/logout',   authController.logout);
