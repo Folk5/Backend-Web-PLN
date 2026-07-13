e262c74140dc18c73f57df28946d67b5cdc27302
FazaFarih <farihid58@gmail.com>
2026-07-13 16:06:59 +0700

    chore: test queries and material fixes

--- query.js
+++ query.js
@@ -1,12 +1,9 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
+const { Client } = require('pg');
+const client = new Client({ connectionString: 'postgresql://postgres:postgres@localhost:5432/pln_db?schema=public' });
 async function main() {
-  const m = await prisma.module.findMany({
-    where: { title: { contains: 'rumah', mode: 'insensitive' } },
-    include: { assets: true }
-  });
-  console.log(JSON.stringify(m, null, 2));
+  await client.connect();
+  const res = await client.query("SELECT * FROM \"ModuleMaterial\" WHERE \"moduleId\" = 'f3ed4576-c0a0-4601-9f52-db483eb87cc0'");
+  console.log('Result length:', res.rows.length);
+  res.rows.forEach(r => console.log('Material ID:', r.materialId, 'meshes:', r.meshes));
 }
-
-main().finally(() => prisma.$disconnect());
+main().catch(console.error).finally(() => client.end());
