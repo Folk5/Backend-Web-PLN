2c17f35d76d314cfbb2c9a26b24d6af45d676493
FazaFarih <farihid58@gmail.com>
2026-08-03 14:53:57 +0700

    chore: add db testing script

---
+++ test_db.js
@@ -0,0 +1,1 @@
+const { Client } = require('pg'); const client = new Client({ connectionString: 'postgresql://postgres:qwerty@localhost:5432/pln_web?schema=public' }); client.connect().then(() => client.query('SELECT id, title, image FROM 'Module' LIMIT 5')).then(res => { console.log(res.rows); client.end(); }).catch(e => { console.error(e); client.end(); });
